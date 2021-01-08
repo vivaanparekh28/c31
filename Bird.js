162e9de54e2dba5bc4d9f6c2d03dfe7f6caea5c0
vivaanparekh28 <vivaanparekh28@gmail.com>
2021-01-08 18:11:24 +0000

    array

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeimage=loadImage("./sprites/smoke.png")
+    this.path=[]
   }
 
   display() {
@@ -9,5 +11,15 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+if (this.body.position.x>220 && this.body.velocity.x>10 ){
+  var pos=[this.body.position.x,this.body.position.y]
+
+  this.path.push(pos)
+}
+    
+
+    for (var i=0;i<this.path.length;i++){
+    image(this.smokeimage,this.path[i][0],this.path[i][1])
+    }
   }
 }
